a22a9e83c2c8b2705d063e5e3755f558ff46fae5
aptonic <contact@aptonic.com>
2023-06-07 20:48:20 +1200

    Remove auth

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,8 +1,6 @@
+import { Session } from "inspector";
 import Navbar from "./navbar";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Nav() {
-  const session = await getServerSession(authOptions);
-  return <Navbar session={session} />;
+  return <Navbar />;
 }
