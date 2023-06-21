a8eb740580ec458b20cb1d1ec09a90cac311771c
aptonic <contact@aptonic.com>
2023-06-22 00:17:43 +1200

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import appIcon from "/public/app_icon.png";
 import Link from "next/link";
 import Balancer from "react-wrap-balancer";
+import DzReviewCard from "@/components/home/dzreview-card";
 
 export default async function Home() {
   return (
@@ -62,7 +63,7 @@ export default async function Home() {
       </div>
 
       <div
-        className="mt-10  w-full flex animate-fade-up justify-center px-8 text-center opacity-0"
+        className="mt-10  flex w-full animate-fade-up justify-center px-8 text-center opacity-0"
         style={{
           animationDelay: "0.55s",
           animationFillMode: "forwards",
@@ -153,9 +154,26 @@ export default async function Home() {
               Free to use and includes a 14 day Dropzone 4 Pro trial
             </li>
             <li className="py-2">
-              Unlock all Dropzone 4 Pro features by <a href="#!" className="paddle_button text-blue-400 underline" data-product="613556" data-theme="none">purchasing Dropzone 4 Pro
-              Lifetime</a> for $40 or <a href="#!" className="paddle_button text-blue-400 underline" data-product="620999" data-theme="none" data-init="true">start a Dropzone 4 Pro subscription</a> for only
-              $1.99/month
+              Unlock all Dropzone 4 Pro features by{" "}
+              <a
+                href="#!"
+                className="paddle_button text-blue-400 underline"
+                data-product="613556"
+                data-theme="none"
+              >
+                purchasing Dropzone 4 Pro Lifetime
+              </a>{" "}
+              for $40 or{" "}
+              <a
+                href="#!"
+                className="paddle_button text-blue-400 underline"
+                data-product="620999"
+                data-theme="none"
+                data-init="true"
+              >
+                start a Dropzone 4 Pro subscription
+              </a>{" "}
+              for only $1.99/month
             </li>
             <li className="py-2">
               Up to 30% discount if upgrading from Dropzone 3. Redeem your
@@ -195,32 +213,33 @@ export default async function Home() {
 
         <div className="mx-auto max-w-screen-xl text-sm">
           <div className="-mx-2 flex flex-wrap">
-            <div className="w-full flex justify-center md:px-5 py-5 text-center md:w-1/3">
-              <div className="min-h-full block max-w-sm rounded-lg border border-gray-200 bg-white p-6  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div className="flex w-full justify-center py-5 text-center md:w-1/3 md:px-5">
+              <div className="block min-h-full max-w-sm rounded-lg border border-gray-200 bg-white p-6  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <p className="font-roboto text-gray-700">
-                  &ldquo;Now that the best parts of Dropzone 4 are free, there&apos;s
-                  simply no reason not to install this must-have menubar
-                  utility.&rdquo;
+                  &ldquo;Now that the best parts of Dropzone 4 are free,
+                  there&apos;s simply no reason not to install this must-have
+                  menubar utility.&rdquo;
                 </p>
               </div>
             </div>
-            <div className="w-full flex justify-center md:px-5 py-5 text-center md:w-1/3">
-              <div className="min-h-full block max-w-sm rounded-lg border border-gray-200 bg-white p-6 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <p className="font-roboto text-gray-700">
-                &ldquo;Dropzone catapults drag and drop into the twenty-first century
-                with one-stop-shop access to files and applications, timesaving
-                actions, and effortless menu bar access. Install it, and you&apos;ll
-                forget all about your Mac&apos;s Dock and Launchpad.&rdquo;
+            <div className="flex w-full justify-center py-5 text-center md:w-1/3 md:px-5">
+              <div className="block min-h-full max-w-sm rounded-lg border border-gray-200 bg-white p-6 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <p className="font-roboto text-gray-700">
+                  &ldquo;Dropzone catapults drag and drop into the twenty-first
+                  century with one-stop-shop access to files and applications,
+                  timesaving actions, and effortless menu bar access. Install
+                  it, and you&apos;ll forget all about your Mac&apos;s Dock and
+                  Launchpad.&rdquo;
                 </p>
               </div>
             </div>
-            <div className="w-full flex justify-center md:px-5 py-5 text-center md:w-1/3">
-              <div className="min-h-full block max-w-sm rounded-lg border border-gray-200 bg-white p-6 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div className="flex w-full justify-center py-5 text-center md:w-1/3 md:px-5">
+              <div className="block min-h-full max-w-sm rounded-lg border border-gray-200 bg-white p-6 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <p className="font-roboto text-gray-700">
-                  &ldquo;Dropzone provides fantastic shortcuts for moving, copying,
-                  uploading files and performing various tasks. Everything is
-                  handled via drag-and-drop, saving you time with common, tedious
-                  tasks.&rdquo;
+                  &ldquo;Dropzone provides fantastic shortcuts for moving,
+                  copying, uploading files and performing various tasks.
+                  Everything is handled via drag-and-drop, saving you time with
+                  common, tedious tasks.&rdquo;
                 </p>
               </div>
             </div>
@@ -232,11 +251,13 @@ export default async function Home() {
         </div>
 
         <div className="mx-auto max-w-screen-xl text-center">
-          <h1 className="text-3xl mb-10 font-roboto">Dropzone makes it faster and easier to:</h1>
+          <h1 className="mb-10 font-roboto text-3xl">
+            Dropzone makes it faster and easier to:
+          </h1>
         </div>
 
-        <div className="mx-auto flex max-w-screen-xl flex-wrap px-2 text-sm text-gray-500 whitespace-nowrap">
-          <div className="w-full px-10 md:py-5 text-center md:w-1/3">
+        <div className="mx-auto flex max-w-screen-xl flex-wrap whitespace-nowrap px-2 text-sm text-gray-500">
+          <div className="w-full px-10 text-center md:w-1/3 md:py-5">
             <ul>
               <li className="flex items-center py-1">
                 <Image
@@ -263,8 +284,12 @@ export default async function Home() {
                   width={50}
                   height={50}
                 />
-                <span className="pl-3 lg:block sm:block md:hidden">Install Applications</span>
-                <span className="pl-3 hidden sm:hidden md:block lg:hidden">Install Apps</span>
+                <span className="pl-3 sm:block md:hidden lg:block">
+                  Install Applications
+                </span>
+                <span className="hidden pl-3 sm:hidden md:block lg:hidden">
+                  Install Apps
+                </span>
               </li>
               <li className="flex items-center py-1">
                 <Image
@@ -273,8 +298,12 @@ export default async function Home() {
                   width={50}
                   height={50}
                 />
-                <span className="pl-3 lg:block sm:block md:hidden">Launch Applications</span>
-                <span className="pl-3 hidden sm:hidden md:block lg:hidden">Launch Apps</span>
+                <span className="pl-3 sm:block md:hidden lg:block">
+                  Launch Applications
+                </span>
+                <span className="hidden pl-3 sm:hidden md:block lg:hidden">
+                  Launch Apps
+                </span>
               </li>
               <li className="flex items-center py-1">
                 <Image
@@ -288,7 +317,7 @@ export default async function Home() {
             </ul>
           </div>
 
-          <div className="w-full px-10 md:py-5 text-center md:w-1/3">
+          <div className="w-full px-10 text-center md:w-1/3 md:py-5">
             <ul>
               <li className="flex items-center py-1">
                 <Image
@@ -338,7 +367,7 @@ export default async function Home() {
             </ul>
           </div>
 
-          <div className="w-full px-10 md:py-5 text-center md:w-1/3">
+          <div className="w-full px-10 text-center md:w-1/3 md:py-5">
             <ul>
               <li className="flex items-center py-1">
                 <Image
@@ -390,15 +419,228 @@ export default async function Home() {
         </div>
 
         <div className="mb-3 mt-10 text-center text-lg text-gray-500">
-          <p>Boost your productivity and complete everyday tasks faster with Dropzone.</p>
+          <p>
+            Boost your productivity and complete everyday tasks faster with
+            Dropzone.
+          </p>
         </div>
 
         <div className="px-5">
           <hr className="my-10 h-px border-t-0 bg-gray-200 opacity-100 dark:opacity-50" />
         </div>
 
+        <div className="mx-auto max-w-screen-xl text-center">
+          <h1 className="mb-12 font-roboto text-3xl">How to use Dropzone</h1>
+        </div>
 
+        <div className="mx-auto max-w-screen-xl text-center">
+          <h1 className="mb-3 font-roboto">
+            1. Drag a file to the top of your screen
+          </h1>
 
+          <Image
+            src="/how-to-use1.png"
+            alt="Drag a file to the top of your screen"
+            width={589}
+            className="mx-auto"
+            height={140}
+          />
+
+          <h1 className="mb-3 mt-16 font-roboto">
+            2. A grid of your actions opens. Drop the file on the action you
+            want
+          </h1>
+
+          <Image
+            src="/how-to-use2.png"
+            alt="A grid of your actions opens. Drop the file on the action you want"
+            width={589}
+            className="mx-auto"
+            height={457}
+          />
+
+          <h1 className="mb-3 mt-16 font-roboto">
+            3. The action is performed on your file
+          </h1>
+
+          <Image
+            src="/how-to-use3.png"
+            alt="The action is performed on your file"
+            width={589}
+            className="mx-auto"
+            height={140}
+          />
+        </div>
+
+        <div className="px-5">
+          <hr className="my-10 h-px border-t-0 bg-gray-200 opacity-100 dark:opacity-50" />
+        </div>
+
+        <div className="mx-auto max-w-screen-xl text-center">
+          <h1 className="mb-10 font-roboto text-3xl">Keyboard shortcuts</h1>
+
+          <h1 className="mb-3 font-roboto">1. Press F3</h1>
+
+          <Image
+            src="/f3.png"
+            alt="Press F3"
+            className="mx-auto"
+            width={141}
+            height={101}
+          />
+
+          <h1 className="mb-3 mt-16 font-roboto">
+            2. Dropzone opens and shows the shortcut key overlaid on each icon
+          </h1>
+
+          <Image
+            src="/keyboard-overlays.png"
+            alt="Dropzone opens and shows the shortcut key overlaid on each icon"
+            width={615}
+            className="mx-auto"
+            height={555}
+          />
+
+          <h1 className="mb-3 mt-16 font-roboto">
+            3. Hit the shortcut key for the action you want to run
+          </h1>
+        </div>
+
+        <div className="px-5">
+          <hr className="my-10 h-px border-t-0 bg-gray-200 opacity-100 dark:opacity-50" />
+        </div>
+
+        <div className="mx-auto max-w-screen-xl text-center">
+          <h1 className="mb-12 font-roboto text-3xl">
+            What people are saying about Dropzone
+          </h1>
+        </div>
+
+        <DzReviewCard
+          name={"Wairami from United States"}
+          src={"Mac App Store Review"}
+        >
+          I literally use this every hour I'm at my laptop... Which is roughly
+          14 hours a day. This app is game-changing for me. I know it has uses
+          other than "store it here to paste where you want later", but that's
+          mainly what I use it for, and man... it does it's job superbly. I
+          can't recommend this app enough for MacBook users.
+        </DzReviewCard>
+
+        <DzReviewCard
+          name={"Ronny411 from United Kingdom"}
+          src={"Mac App Store Review"}
+        >
+          Such a handy little app, if you like a clean and organised working
+          environment. I study and work full time; this lets me organise things
+          rapidly and without any fuss. Couldn’t recommend it more.
+        </DzReviewCard>
+
+        <DzReviewCard
+          name={"DrumGuy68 from United States"}
+          src={"Mac App Store Review"}
+        >
+          This app is great for streamlining tasks that you would do on your Mac
+          everyday. It also helps keeps the desktop clutter down as you can drag
+          an drop whatever you're working with and place it in with out having
+          to open up a bunch of folders to get to it. (Pre-setup is required)
+          Once you have it setup, it's a snap.
+        </DzReviewCard>
+
+        <DzReviewCard
+          name={"donkey2thekong from United States"}
+          src={"Mac App Store Review"}
+        >
+          This App has changed the way I do everything!! It is hands down the
+          best app on the market and is the now the app against which I rate all
+          others. Dropzone is a 6 for sure!!
+        </DzReviewCard>
+
+        <DzReviewCard
+          name={"barryforque from United States"}
+          src={"Mac App Store Review"}
+        >
+          Great, handsome and easy to use. I love the different Dropzone
+          functions like automatic copying to folders and automatic moving to
+          others. I use it to do no-fuss backups of work files using my Dropbox
+          account. Way simpler than Finder or keeping shortcuts on the desktop.
+        </DzReviewCard>
+
+        <DzReviewCard
+          name={"german1608 from Panama"}
+          src={"Mac App Store Review"}
+        >
+          This is one of the apps that once you use it you can't live without
+          it. It saves a bunch of steps when moving files around. Highly
+          recommended.
+        </DzReviewCard>
+
+        <div className="px-5">
+          <hr className="my-10 h-px border-t-0 bg-gray-200 opacity-100 dark:opacity-50" />
+        </div>
+
+        <div className="mx-auto max-w-screen-xl text-center">
+          <h1 className="mb-10 font-roboto text-3xl">There's more...</h1>
+        </div>
+
+        <div className="mx-auto max-w-screen-xl text-center">
+          <Image
+            src="/devicon.png"
+            alt="Dropzone Action Development Icon"
+            className="mx-auto"
+            width={136}
+            height={133}
+          />
+
+          <div className="mb-3 mt-16 text-center text-base text-gray-500">
+            <p>
+              Easily extend Dropzone by installing more actions in just a few
+              clicks.
+            </p>
+
+            <br />
+
+            <p>Develop your own Dropzone actions in Ruby or Python.</p>
+            <p>
+              You can also modify existing actions and share your actions with
+              others.
+            </p>
+
+            <br />
+
+            <p>We can't wait to see what you make.</p>
+          </div>
+
+          <div className="mx-auto mt-12 flex max-w-md text-center text-base font-bold text-blue-500 justify-center">
+            <Link href="/actions">
+              <div className="mx-auto mr-10 flex items-center">
+                <svg
+                  className="mr-2 flex w-7 fill-blue-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z" />
+                </svg>
+                <span className="flex">Install Actions</span>
+              </div>
+            </Link>
+
+            <Link href="https://github.com/aptonic/dropzone4-actions/blob/master/README.md#introducing-the-dropzone-4-api">
+              <div className="mx-auto flex items-center">
+                <svg
+                  className="mr-2 flex w-5 fill-blue-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M448 360V24c0-13.3-10.7-24-24-24H96C43 0 0 43 0 96v320c0 53 43 96 96 96h328c13.3 0 24-10.7 24-24v-16c0-7.5-3.5-14.3-8.9-18.7-4.2-15.4-4.2-59.3 0-74.7 5.4-4.3 8.9-11.1 8.9-18.6zM128 134c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm0 64c0-3.3 2.7-6 6-6h212c3.3 0 6 2.7 6 6v20c0 3.3-2.7 6-6 6H134c-3.3 0-6-2.7-6-6v-20zm253.4 250H96c-17.7 0-32-14.3-32-32 0-17.6 14.4-32 32-32h285.4c-1.9 17.1-1.9 46.9 0 64z" />
+                </svg>
+                <span className="flex">Developer Documentation</span>
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        <br />
         <br />
         <br />
       </div>
---
+++ components/home/dzreview-card.tsx
@@ -0,0 +1,94 @@
+export default function DzReviewCard({
+  children,
+  name,
+  src,
+}: {
+  children: React.ReactNode;
+  name: string;
+  src: string;
+}) {
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-3xl">
+        <div className="m-4 block rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-800 dark:shadow-black/20">
+          <div className="md:flex md:flex-row">
+            <div className="md:ml-6">
+              <p className="mb-6 font-light text-neutral-500 dark:text-neutral-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  className="inline-block h-7 w-7 pr-2"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"></path>
+                </svg>
+                {children}
+              </p>
+              <div className="rating">
+                <div className="mb-5 flex items-center">
+                  <svg
+                    aria-hidden="true"
+                    className="h-7 w-7 text-indigo-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>First star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="h-7 w-7 text-indigo-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Second star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="h-7 w-7 text-indigo-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Third star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="h-7 w-7 text-indigo-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Fourth star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    className="h-7 w-7 text-indigo-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Fifth star</title>
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                </div>
+              </div>
+
+              <p className="mb-2 text-base font-semibold text-neutral-800 dark:text-neutral-200">
+                {name}
+              </p>
+              <p className="mb-0 text-sm font-semibold text-neutral-500 dark:text-neutral-400">
+                {src}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
