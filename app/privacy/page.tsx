b996b7ac11ff11f48a409fd19114f344f8d316c6
aptonic <contact@aptonic.com>
2023-07-19 23:37:40 +1200

    added terms and privacy policy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,149 @@
+
+export default function Page() {
+  return (
+    <div className="z-10 mt-28 px-5 sm:w-full md:w-10/12 lg:w-1/2 ">
+      <h2 className="mb-10 text-xl font-medium text-gray-700">Privacy Policy</h2>
+      <div className="text-gray-700">
+
+        <p className="mb-5">
+          This page is used to inform website visitors regarding our policies
+          with the collection, use, and disclosure of Personal Information if
+          anyone decided to use our Service, the Aptonic website.
+        </p>
+
+        <p className="mb-5">
+          If you choose to use our Service, then you agree to the collection and
+          use of information in relation with this policy. The Personal
+          Information that we collect are used for providing and improving the
+          Service. We will not use or share your information with anyone except
+          as described in this Privacy Policy.
+        </p>
+
+        <p className="mb-5">
+          The terms used in this Privacy Policy have the same meanings as in our
+          Terms and Conditions, which is accessible at{' '}
+          <a className="underline" href="/terms">https://aptonic.com/terms</a>, unless otherwise defined in this
+          Privacy Policy.
+        </p>
+
+        <h2 className="text-lg mb-5 mt-8 font-medium">Our Use of Google User Data</h2>
+        <p className="mb-5">
+          Our Mac Application Dropzone allows you to connect with your account
+          on Google Drive. Dropzone uses OAuth to obtain a token to allow it to
+          access your Google Drive solely for the purpose of allowing you to
+          upload files into your own drive. None of your Google Drive data is
+          stored by us or transmitted beyond your own machine by Dropzone. In
+          some cases Dropzone may maintain an internal list of URLs for you to
+          re-copy a link to file you uploaded earlier. This list is stored in a
+          user preferences file on your own computer only. Dropzone also allows
+          linking to a YouTube account - once again for the sole purpose of
+          allowing you to upload videos to YouTube. No personal information
+          obtained via Googles APIs is ever transmitted to us or any third party
+          when using Dropzone.
+        </p>
+
+        <h2 className="text-lg mb-5 mt-8 font-medium">Agreement with YouTube Terms of Service</h2>
+        <p className="mb-5">
+          Dropzone uses YouTube API Services.
+          <br />
+          By using the YouTube Uploader action you are agreeing to be bound by
+          the<br />
+          <a className="text-blue-400 underline" href="https://www.youtube.com/t/terms">
+            YouTube Terms of Service.
+          </a>
+        </p>
+
+        <h2 className="text-lg mb-5 mt-8 font-medium">Information Collection and Use</h2>
+
+        <h2 className="text-base mb-5 mt-8 font-medium">Log Data</h2>
+
+        <p className="mb-5">
+          When you visit our website, we collect information that your browser
+          sends to us that is called Log Data. This Log Data may include
+          information such as your computer's Internet Protocol ("IP") address,
+          browser version, pages of our Service that you visit, the time and
+          date of your visit, the time spent on those pages, and other
+          statistics.
+        </p>
+
+        <h2 className="text-base mb-5 mt-8 font-medium">Cookies</h2>
+
+        <p className="mb-5">
+          Cookies are files with small amount of data that is commonly used an
+          anonymous unique identifier. These are sent to your browser from the
+          website that you visit and are stored on your computer's hard drive.
+        </p>
+
+        <p className="mb-5">
+          Our website uses these "cookies" to collection information and to
+          improve our Service. You have the option to either accept or refuse
+          these cookies, and know when a cookie is being sent to your computer.
+          If you choose to refuse our cookies, you may not be able to use some
+          portions of our Service.
+        </p>
+
+        <h2 className="text-base mb-5 mt-8 font-medium">Service Providers</h2>
+
+        <p className="mb-5">
+          We may employ third-party companies and individuals due to the
+          following reasons:
+        </p>
+
+        <ul>
+          <li>To facilitate our Service;</li>
+          <li>To provide the Service on our behalf;</li>
+          <li>To perform Service-related services; or</li>
+          <li>To assist us in analyzing how our Service is used.</li>
+        </ul>
+
+        <p className="mb-5">
+          We want to inform our Service users that these third parties have
+          access to your Personal Information. The reason is to perform the
+          tasks assigned to them on our behalf. However, they are obligated not
+          to disclose or use the information for any other purpose.
+        </p>
+
+        <h2 className="text-base mb-5 mt-8 font-medium">Security</h2>
+
+        <p>
+          We value your trust in providing us your Personal Information, thus we
+          are striving to use commercially acceptable means of protecting it.
+          But remember that no method of transmission over the internet, or
+          method of electronic storage is 100% secure and reliable, and we
+          cannot guarantee its absolute security.
+        </p>
+
+        <h2 className="text-base mb-5 mt-8 font-medium">Links to Other Sites</h2>
+
+        <p className="mb-5">
+          Our Service may contain links to other sites. If you click on a
+          third-party link, you will be directed to that site. Note that these
+          external sites are not operated by us. Therefore, we strongly advise
+          you to review the Privacy Policy of these websites. We have no control
+          over, and assume no responsibility for the content, privacy policies,
+          or practices of any third-party sites or services.
+        </p>
+
+        <h2 className="text-base mb-5 mt-8 font-medium">Changes to This Privacy Policy</h2>
+
+        <p className="mb-5">
+          We may update our Privacy Policy from time to time. Thus, we advise
+          you to review this page periodically for any changes. We will notify
+          you of any changes by posting the new Privacy Policy on this page.
+          These changes are effective immediately, after they are posted on this
+          page.
+        </p>
+
+        <h2 className="text-base mb-5 mt-8 font-medium">Contact Us</h2>
+
+        <p className="mb-5">
+          If you have any questions about our Privacy Policy, do
+          not hesitate to contact us.
+        </p>
+
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+}
---
+++ app/terms/page.tsx
@@ -0,0 +1,121 @@
+
+export default function Page() {
+  return (
+      <div className="z-10 mt-28 px-5 sm:w-full md:w-10/12 lg:w-1/2">
+        <h2 className="mb-10 text-xl font-medium text-gray-700">Dropzone 4 Terms of Service</h2>
+
+        <div className="text-gray-700">
+          <p className="mb-5">
+            By using the YouTube Uploader API Client, you are agreeing to be
+            bound by the{" "}
+            <a className="underline" href="https://www.youtube.com/t/terms">
+              YouTube Terms of Service.
+            </a>
+          </p>
+
+          <p className="mb-5">
+            <strong>Subscription:</strong> Dropzone 4 provides an optional
+            subscription. This subscription includes access to our growing
+            library of cloud based actions, regular cloud action updates and
+            enhancements, as well as all app updates, including major ones,
+            provided automatically. It all includes access to all new features
+            added in updates and unlocks Dropzone's advanced features including
+            Amazon S3 Uploading, Google Drive Uploading, FTP/SFTP Uploading,
+            macOS service integration and more.
+          </p>
+
+          <p className="mb-5">
+            Launch Dropzone and choose 'Subscribe to Dropzone 4 Pro...' from the
+            settings menu to begin your 14-day free trial. You can cancel during
+            the free trial at any point, and you won't be charged for using
+            Dropzone 4.
+          </p>
+
+          <p className="mb-5">
+            After 14 days, you'll automatically be billed 1.99USD per month (or
+            the equivalent in your country) through an auto-renewing
+            subscription.
+          </p>
+
+          <p className="mb-5">
+            An active subscription entitles you to unlimited Dropzone 4 app
+            updates, including both minor updates and all major new features.
+          </p>
+
+          <p className="mb-5">
+            You can cancel your subscription at any time. If you do, Dropzone
+            cloud actions and the advanced features detailed above will no
+            longer be available. You can restart your subscription at any time
+            to continue using Dropzone cloud actions and the advanced features
+            detailed above.
+          </p>
+
+          <p className="mb-5">
+            You, the subscriber, assume responsibility for the selection of the
+            program Dropzone 4 to achieve your intended results, and for the
+            installation, use, and results obtained from the program.
+            Installation and use of the program constitutes your acceptance of
+            these terms and conditions. If you do not accept these terms and
+            conditions, then do not use the program, and remove it from your
+            computer.
+          </p>
+
+          <p className="mb-5">
+            <strong>License:</strong> You may use the program on any and all
+            computers linked to your Apple account, provided that no copy of the
+            program and documentation may be used by anyone other than you. You
+            may not use or copy the program or documentation, or any copy
+            thereof, in whole or in part, except as provided in this Agreement.
+            You also may not modify or transfer (whether or not for
+            consideration) the program or documentation, or any copy thereof, in
+            whole or in part.
+          </p>
+
+          <p className="mb-5">
+            <strong>Term:</strong> The license is effective on the date you
+            accept this Agreement, and remains in effect until terminated as
+            indicated above or until you terminate it. If the license is
+            terminated for any reason, you agree to destroy the program and
+            documentation, together with all copies thereof, in whole or in
+            part, in any form, and to cease all use of the program and
+            documentation.
+          </p>
+
+          <p className="mb-5">
+            <strong>Limited Warranty and Limitation of Remedies:</strong> The
+            program, documentation and any support from Aptonic Limited, are
+            provided “as is” and without warranty, express and implied,
+            including but not limited to any implied warranties of
+            merchantability and fitness for a particular purpose. In no event
+            will Aptonic Limited. be liable for any damages, including lost
+            profits, lost savings, or other incidental or consequential damages,
+            even if Aptonic Limited is advised of the possibility of such
+            damages, or for any claim by you or any third party.
+          </p>
+
+          <p className="mb-5">
+            <strong>General Terms:</strong> This Agreement can only be modified
+            by a written agreement signed by you and Aptonic Limited and changes
+            from the terms and conditions of this Agreement made in any other
+            manner will be of no effect. If any portion of this Agreement shall
+            be held invalid, illegal, or unenforceable, the validity, legality,
+            and enforceability of the remainder of the Agreement shall not in
+            any way be affected or impaired thereby.
+          </p>
+
+          <p className="mb-5">
+            <strong>Acknowledgment:</strong> You acknowledge that you have read
+            this agreement, understand it, and agree to be bound by its terms
+            and conditions. You further agree that it is the complete and
+            exclusive statement of the agreement between you and Aptonic
+            Limited. which supersedes all proposals or prior agreements, oral or
+            written, and all other communications between you and Aptonic
+            Limited. relating to the subject matter of this agreement.
+          </p>
+
+          <br />
+          <br />
+        </div>
+      </div>
+  );
+}
