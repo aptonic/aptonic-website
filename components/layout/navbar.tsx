1b8553ed5d7c26736406363817d8d3ca3605a855
aptonic <contact@aptonic.com>
2023-07-21 17:47:56 +1200

    update navbar.tsx

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import Aptonic from "../shared/icons/aptonic";
 import { usePathname } from "next/navigation";
 
@@ -15,27 +14,27 @@ export default function NavBar({ isBlog }) {
     <>
       <div className="fixed top-0 z-20 flex w-full justify-center border-b border-gray-200 bg-white/50 backdrop-blur-xl">
         <div className={containerClassName}>
-          <Link href="/" className="flex items-center font-display text-2xl">
+          <a href="/" className="flex items-center font-display text-2xl">
             <Aptonic className="ml-5" />
-          </Link>
+          </a>
           <div className="mr-10 flex justify-end gap-10 md:mr-20 md:gap-20">
-            <Link
+            <a
               href="/"
               className={`invisible w-[46px] sm:visible ${
                 usePathname() == "/" ? "font-bold " : "font-normal"
               }`}
             >
               Home
-            </Link>
-            <Link
+            </a>
+            <a
               href="/blog"
               className={`w-[32px] ${
                 pathname?.startsWith("/blog") ? "font-bold" : "font-normal"
               }`}
             >
               Blog
-            </Link>
-            <Link
+            </a>
+            <a
               href="/support"
               className={`w-[58px] ${
                 pathname?.startsWith("/support") ||
@@ -49,7 +48,7 @@ export default function NavBar({ isBlog }) {
               }`}
             >
               Support
-            </Link>
+            </a>
           </div>
         </div>
       </div>
