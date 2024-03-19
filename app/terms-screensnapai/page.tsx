8595b98eeda0f80470339256041e0a18a6c1e36e
aptonic <contact@aptonic.com>
2024-03-20 11:40:22 +1300

    Add ScreenSnapAI terms

---
+++ app/terms-screensnapai/page.tsx
@@ -0,0 +1,70 @@
+export const metadata = {
+  title: "Aptonic - Terms of Service",
+};
+
+export default function Page() {
+  return (
+      <div className="z-10 mt-28 px-5 sm:w-full lg:max-w-4xl">
+        <h2 className="mb-10 text-xl font-medium text-gray-700">ScreenSnapAI Terms of Service</h2>
+
+        <div className="text-gray-700">
+
+          <p className="mb-5">
+            <strong>License:</strong> You may use the program on any and all
+            computers linked to your Apple account, provided that no copy of the
+            program and documentation may be used by anyone other than you. You
+            may not use or copy the program or documentation, or any copy
+            thereof, in whole or in part, except as provided in this Agreement.
+            You also may not modify or transfer (whether or not for
+            consideration) the program or documentation, or any copy thereof, in
+            whole or in part.
+          </p>
+
+          <p className="mb-5">
+            <strong>Term:</strong> The license is effective on the date you
+            accept this Agreement, and remains in effect until terminated as
+            indicated above or until you terminate it. If the license is
+            terminated for any reason, you agree to destroy the program and
+            documentation, together with all copies thereof, in whole or in
+            part, in any form, and to cease all use of the program and
+            documentation.
+          </p>
+
+          <p className="mb-5">
+            <strong>Limited Warranty and Limitation of Remedies:</strong> The
+            program, documentation and any support from Aptonic Limited, are
+            provided “as is” and without warranty, express and implied,
+            including but not limited to any implied warranties of
+            merchantability and fitness for a particular purpose. In no event
+            will Aptonic Limited. be liable for any damages, including lost
+            profits, lost savings, or other incidental or consequential damages,
+            even if Aptonic Limited is advised of the possibility of such
+            damages, or for any claim by you or any third party.
+          </p>
+
+          <p className="mb-5">
+            <strong>General Terms:</strong> This Agreement can only be modified
+            by a written agreement signed by you and Aptonic Limited and changes
+            from the terms and conditions of this Agreement made in any other
+            manner will be of no effect. If any portion of this Agreement shall
+            be held invalid, illegal, or unenforceable, the validity, legality,
+            and enforceability of the remainder of the Agreement shall not in
+            any way be affected or impaired thereby.
+          </p>
+
+          <p className="mb-5">
+            <strong>Acknowledgment:</strong> You acknowledge that you have read
+            this agreement, understand it, and agree to be bound by its terms
+            and conditions. You further agree that it is the complete and
+            exclusive statement of the agreement between you and Aptonic
+            Limited. which supersedes all proposals or prior agreements, oral or
+            written, and all other communications between you and Aptonic
+            Limited. relating to the subject matter of this agreement.
+          </p>
+
+          <br />
+          <br />
+        </div>
+      </div>
+  );
+}
